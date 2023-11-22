59f308dd187472a7433e0abfa1f8b6c60d9f295c
NekodaMushi <study.fabien@gmail.com>
2023-11-22 17:50:01 +0100

    Auto-commit on Wed Nov 22 05:50:01 PM CET 2023

--- 0x01-travel-list/src/App.js
+++ 0x01-travel-list/src/App.js
@@ -24,17 +24,18 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(5);
 
   function handleSubmit(e) {
     e.preventDefault();
-    initialItems.append(setDescription());
-    console.log(e);
+    // initialItems.append(setDescription());
+    // console.log(e);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip ?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
