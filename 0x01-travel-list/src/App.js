063e1d68498c422db523a9888b7cc8e6c1151b07
NekodaMushi <study.fabien@gmail.com>
2023-11-22 14:30:01 +0100

    Auto-commit on Wed Nov 22 02:30:01 PM CET 2023

--- 0x01-travel-list/src/App.js
+++ 0x01-travel-list/src/App.js
@@ -21,10 +21,12 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
-  function handleSubmit
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip ?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
