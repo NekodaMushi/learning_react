113ebe5c6c5a75b4b7338e79f6e9e51ce445b967
NekodaMushi <study.fabien@gmail.com>
2023-11-28 10:20:01 +0100

    Auto-commit on Tue Nov 28 10:20:01 AM CET 2023

--- 0x01-travel-list/src/App.js
+++ 0x01-travel-list/src/App.js
@@ -8,11 +8,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -22,7 +27,7 @@ function Logo() {
   return <h1>🌴 Far Away 🌐</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(3);
 
@@ -32,6 +37,8 @@ function Form() {
     // console.log(e);
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
   }
 
   return (
@@ -55,11 +62,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div>
       <ul className="list">
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item key={item.id} item={item} />
         ))}
       </ul>
