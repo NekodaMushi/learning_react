4ce72baab8eb1e2142ceb5ac013231a6d191203b
NekodaMushi <study.fabien@gmail.com>
2023-11-29 11:30:01 +0100

    Auto-commit on Wed Nov 29 11:30:01 AM CET 2023

--- 0x01-travel-list/src/App.js
+++ 0x01-travel-list/src/App.js
@@ -96,11 +96,15 @@ function PackingList({ items, onRemove, onPacked }) {
   let sortedItems;
 
   if (sortBy === "input") sortedItems = items;
-  // if (sortBy === "packed")
+
   if (sortBy === "description")
     sortedItems = items
       .slice()
       .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div>
       <ul
@@ -119,9 +123,10 @@ function PackingList({ items, onRemove, onPacked }) {
         <div className="actions">
           <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
             <option value="input">Sort by input order</option>
-            <option value="description">Sort by description</option>
+            <option value="description">Sort by alphabetic</option>
             <option value="packed">Sort by packed status</option>
           </select>
+          <button>Clear List</button>
         </div>
       </ul>
     </div>
