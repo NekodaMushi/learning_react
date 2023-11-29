9e705e7530a86ac013d5ccfbcc03e902edd66848
NekodaMushi <study.fabien@gmail.com>
2023-11-29 11:20:01 +0100

    Auto-commit on Wed Nov 29 11:20:01 AM CET 2023

--- 0x01-travel-list/src/App.js
+++ 0x01-travel-list/src/App.js
@@ -96,6 +96,11 @@ function PackingList({ items, onRemove, onPacked }) {
   let sortedItems;
 
   if (sortBy === "input") sortedItems = items;
+  // if (sortBy === "packed")
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
   return (
     <div>
       <ul
@@ -112,7 +117,7 @@ function PackingList({ items, onRemove, onPacked }) {
         ))}
 
         <div className="actions">
-          <select value={sortBy}>
+          <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
             <option value="input">Sort by input order</option>
             <option value="description">Sort by description</option>
             <option value="packed">Sort by packed status</option>
