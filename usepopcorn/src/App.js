5d7597e8603756dab2b6c73cc341b7757653d99e
NekodaMushi <study.fabien@gmail.com>
2024-01-18 16:00:02 +0100

    Auto-commit on Thu Jan 18 04:00:02 PM CET 2024

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -4,18 +4,16 @@
 import { useEffect, useRef, useState } from "react";
 import SpinnerSVG from "./Assets/tail-spin.svg";
 import StarRating from "./StarRating";
+import { useMovies } from "./Components/useMovies";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "7b15cb43";
-
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  const { movies, isLoading, error } = useMovies(query);
 
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
@@ -46,53 +44,6 @@ export default function App() {
     },
     [watched]
   );
-
-  useEffect(
-    function () {
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-          const res = await fetch(
-            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok) {
-            throw new Error("Something went wrong with fetching movies       ");
-          }
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      // if (query.length > 3) fetchMovies();
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      } // Best practice
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  ); //empty array means that the effect will only be executed after the component first mount
 
   return (
     <>
