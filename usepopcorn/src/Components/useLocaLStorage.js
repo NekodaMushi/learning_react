f39d3f17ee08f107f7f0820d0fe22219f97e60a6
NekodaMushi <study.fabien@gmail.com>
2024-01-19 08:50:02 +0100

    Auto-commit on Fri Jan 19 08:50:02 AM CET 2024

--- usepopcorn/src/Components/useLocaLStorage.js
+++ usepopcorn/src/Components/useLocaLStorage.js
@@ -1,14 +1,14 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useLocalStorage(initialState, key) {
   const [value, setValue] = useState(function () {
-    const storedValue = localStorage.getItem("key");
+    const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialState;
   });
 
   useEffect(
     function () {
-      localStorage.setItem(key, JSON.stringify(value));
+      localStorage.setItem("watched", JSON.stringify(value));
     },
     [value, key]
   );
