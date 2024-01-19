323768794ed24295796028b78480d3f0184bafc7
NekodaMushi <study.fabien@gmail.com>
2024-01-19 09:30:01 +0100

    Auto-commit on Fri Jan 19 09:30:01 AM CET 2024

--- usepopcorn/src/Components/useLocalStorage.js
+++ usepopcorn/src/Components/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function useLocaLStorage(initialState, key) {
+export function useLocalStorage(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialState;
