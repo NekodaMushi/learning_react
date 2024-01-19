8a28a56a5ecf403b8ceedfe2cf82080af2c18b73
NekodaMushi <study.fabien@gmail.com>
2024-01-19 09:20:01 +0100

    Auto-commit on Fri Jan 19 09:20:01 AM CET 2024

--- usepopcorn/src/Components/useLocaLStorage.js
+++ usepopcorn/src/Components/useLocaLStorage.js
@@ -5,4 +5,12 @@ export function useLocaLStorage(initialState, key) {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialState;
   });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+  return [value, setValue];
 }
