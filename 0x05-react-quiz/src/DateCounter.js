875d2a24f515a35b64d8c8a73b04e0b606c8dfd7
NekodaMushi <study.fabien@gmail.com>
2024-01-22 16:30:01 +0100

    Auto-commit on Mon Jan 22 04:30:01 PM CET 2024

--- 0x05-react-quiz/src/DateCounter.js
+++ 0x05-react-quiz/src/DateCounter.js
@@ -1,17 +1,30 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const ACTION = {
+  DECREMENT: "dec",
+  INCREMENT: "inc",
+  SETTING_COUNT: "setCount",
+};
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - action.payload; // state - 1 is alright as well
-  if (action.type === "setCount") return action.payload;
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "dec") return state - action.payload; // state - 1 is alright as well
+  // if (action.type === "setCount") return action.payload;
+  switch (action.type) {
+    case ACTION.DECREMENT:
+      return; {
+        ...state, 
+      }
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
-  const [count, dispatch] = useReducer(reducer, 0);
-
+  // const [step, setStep] = useState(1);
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = initialState;
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
