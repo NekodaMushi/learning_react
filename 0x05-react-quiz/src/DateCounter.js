037639a89e6892d5dd676bb72572f6dd0de75e76
NekodaMushi <study.fabien@gmail.com>
2024-01-22 15:00:01 +0100

    Auto-commit on Mon Jan 22 03:00:01 PM CET 2024

--- 0x05-react-quiz/src/DateCounter.js
+++ 0x05-react-quiz/src/DateCounter.js
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
@@ -8,7 +8,14 @@ function reducer(state, action) {
 
   switch (action.type) {
     case "dec":
+      return { ...state, count: state.count - 1 };
+    case "inc":
+      return { ...state, count: state.count + 1 };
+    case "setCount":
       return {};
+
+    default:
+      throw new Error("Unknown Error");
   }
 }
 
