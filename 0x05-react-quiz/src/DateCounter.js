c6f1f92403ae846d8f1e06dc2a11a081b235d3f3
NekodaMushi <study.fabien@gmail.com>
2024-01-22 11:40:02 +0100

    Auto-commit on Mon Jan 22 11:40:01 AM CET 2024

--- 0x05-react-quiz/src/DateCounter.js
+++ 0x05-react-quiz/src/DateCounter.js
@@ -2,15 +2,21 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - action.payload; // state - 1 is alright as well
-  if (action.type === "setCount") return action.payload;
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "dec") return state - action.payload; // state - 1 is alright as well
+  return { count: 0, step: 1 };
+
+  switch (action.type) {
+    case "dec":
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
-  const [count, dispatch] = useReducer(reducer, 0);
+  // const [step, setStep] = useState(1);
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -35,12 +41,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     dispatch({ type: "setCount", payload: 0 });
-    setStep(1);
+    // setStep(1);
   };
 
   return (
