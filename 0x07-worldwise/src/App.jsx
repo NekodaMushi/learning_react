19c02212c1b1327cb20b3bd3d68d0186cb6fd526
NekodaMushi <study.fabien@gmail.com>
2024-02-02 10:00:01 +0100

    Auto-commit on Fri Feb  2 10:00:01 AM CET 2024

--- 0x07-worldwise/src/App.jsx
+++ 0x07-worldwise/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import Homepage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -10,6 +10,9 @@ function App() {
       <Routes>
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
