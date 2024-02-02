7e196217c6a3202c11c93bde0a82f2913ae08053
NekodaMushi <study.fabien@gmail.com>
2024-02-02 10:20:01 +0100

    Auto-commit on Fri Feb  2 10:20:01 AM CET 2024

---
+++ 0x07-worldwise/src/components/PageNav.jsx
@@ -0,0 +1,19 @@
+function PageNav() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/product">Home</Link>
+        </li>
+        <li>
+          <Link to="/pricing">Home</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default PageNav;
