dbb32c86985b1fe60724b2730a5d15777090c49f
NekodaMushi <study.fabien@gmail.com>
2024-02-07 12:16:17 +0100

    progression updated

--- 0x07-worldwise/src/components/PageNav.jsx
+++ 0x07-worldwise/src/components/PageNav.jsx
@@ -13,6 +13,11 @@ function PageNav() {
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
