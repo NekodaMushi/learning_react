3aa4b9e4c8ff5825e837aef742ad430c6fa8baf2
NekodaMushi <study.fabien@gmail.com>
2024-02-09 10:10:02 +0100

    Auto-commit on Fri Feb  9 10:10:02 AM CET 2024

--- 0x07-worldwise/src/App.jsx
+++ 0x07-worldwise/src/App.jsx
@@ -6,8 +6,30 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const BASE_USRL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsloading(true);
+        const res = await fetch(`${BASE_USRL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsloading(false);
+      }
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
