42bbc8fa56ea32444bd1bf95a9d5b08eb5b6fa35
NekodaMushi <study.fabien@gmail.com>
2024-02-09 10:40:01 +0100

    Auto-commit on Fri Feb  9 10:40:01 AM CET 2024

--- 0x07-worldwise/src/components/CityItem.jsx
+++ 0x07-worldwise/src/components/CityItem.jsx
@@ -1,5 +1,24 @@
-function CityItem() {
-  return <li>city</li>;
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { cityName, country, emoji } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)})</time>
+      <p>{country}</p>
+    </li>
+  );
 }
 
 export default CityItem;
